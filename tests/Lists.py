96f943ca1cfe4dc3bae46ba35973b912bba60216
snakeeyes233 <43482065+snakeeyes233@users.noreply.github.com>
2019-09-29 04:48:51 -0400

    Update Lists.py

--- tests/Lists.py
+++ tests/Lists.py
@@ -9,11 +9,9 @@ class Lists(unittest.TestCase):
         utils = Utils()
         self.assertTrue(utils.list_recent_deaths(
             death_date='24 JUL 1984',
-            today_date='25 JUL 1984',
         ))
         self.assertRaises(ValueError, utils.list_recent_deaths, 
             death_date='22 JUL 2013',
-            today_date='24 JUL 2015', 
             
         )
 
