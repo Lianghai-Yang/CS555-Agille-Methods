42fb853e01dd02c67c6a45a55ef42d1d18a0822d
snakeeyes233 <43482065+snakeeyes233@users.noreply.github.com>
2019-09-29 14:49:48 -0400

    fixed lists

--- tests/Lists.py
+++ tests/Lists.py
@@ -4,11 +4,11 @@ import unittest
 from Utils import Utils
 
 class Lists(unittest.TestCase):
-    pass
+    
     def list_recent_deaths(self):
         utils = Utils()
         self.assertTrue(utils.list_recent_deaths(
-            death_date='24 JUL 1984',
+            person= {'ID':'@I1', 'DEAT':'24 JUL 1984'}
         ))
         self.assertRaises(ValueError, utils.list_recent_deaths, 
             death_date='22 JUL 2013',
