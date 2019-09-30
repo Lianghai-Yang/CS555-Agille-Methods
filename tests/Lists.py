ef0fcd81074b2c815f2e6d8520fede57d5e6ae91
Lianghai-Yang <yanglianghai1994@gmail.com>
2019-09-29 22:22:46 -0400

    fix Lists test

--- tests/Lists.py
+++ tests/Lists.py
@@ -8,11 +8,10 @@ class Lists(unittest.TestCase):
     
     def test_list_recent_deaths(self):
         utils = Utils()
-        before_31_days = (datetime.today() - timedelta(days=20)).strftime(_format)
-        print(before_31_days)
+        before_days = (datetime.today() - timedelta(days=20)).strftime(_format)
         self.assertListEqual(utils.list_recent_deaths(
             people = {
-                '@I1@': {'ID': '@I1@', 'NAME': 'Tim /James/', 'SEX': 'M', 'BIRT': before_31_days, 'FAMC': '@F2@', 'DEAT': '24 SEP 2019', 'HUSB': ['@F1@'], 'CHIL': ['@F2@']},
+                '@I1@': {'ID': '@I1@', 'NAME': 'Tim /James/', 'SEX': 'M', 'BIRT': before_days, 'FAMC': '@F2@', 'DEAT': '24 SEP 2019', 'HUSB': ['@F1@'], 'CHIL': ['@F2@']},
                 '@I2@': {'ID': '@I2@', 'NAME': 'Anna /Bella/', 'SEX': 'F', 'BIRT': '23 JUL 1971', 'FAMC': '@F4@', 'DEAT': '31 MAR 2019', 'WIFE': ['@F1@', '@F3@'], 'CHIL': ['@F4@']},
                 '@I3@': {'ID': '@I2@', 'NAME': 'Anna /Bella/', 'SEX': 'F', 'BIRT': '23 JUL 1971', 'FAMC': '@F4@', 'DEAT': 'N/A', 'WIFE': ['@F1@', '@F3@'], 'CHIL': ['@F4@']},
             }),
